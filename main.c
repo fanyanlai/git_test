20cbb14fe7d82ee79e40161bd1cf9025fd5fee78
Andy Fan <Andy.Fan@flexworks.com>
2020-09-23 10:08:11 +0800

    Revert "add API-sum"
    This reverts commit df351c1197b6568abd603dcba0718d5072da75ad.

--- main.c
+++ main.c
@@ -6,6 +6,12 @@ int main (void)
 	int ret = 0;
 	return ret;
 }
+int max(int a, int b)
+{
+	int ret = 0;
+	ret = a>b ? a : b;
+	return ret;
+}
 
 int sum(int a, int b)
 {
@@ -15,10 +21,4 @@ int sum(int a, int b)
 
 }
 
-int max(int a, int b)
-{
-	int ret = 0;
-	ret = a>b ? a : b;
-	return ret;
-}
 
